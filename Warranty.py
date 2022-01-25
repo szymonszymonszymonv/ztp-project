178ea3cad63822316b612f3eda4e591cd5110039
szymonszymonszymonv <kalarepka8799@gmail.com>
2022-01-25 01:15:40 +0100

    fix original

--- Warranty.py
+++ Warranty.py
@@ -5,6 +5,8 @@ class Warranty(IProduct):
     def __init__(self, product):
         if not hasattr(product, 'original'):
             self.original = product
+        else:
+            self.original = product.original
             
         self._product = product
         self.id = product.id
